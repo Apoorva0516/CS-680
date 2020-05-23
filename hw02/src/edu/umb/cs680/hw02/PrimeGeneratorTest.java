5f494e7d5586ddd3b92acceb0c81285bd45d3ffc
Apoorva0516 <38570180+Apoorva0516@users.noreply.github.com>
2020-05-23 15:58:06 -0400

    Add files via upload

---
+++ hw02/src/edu/umb/cs680/hw02/PrimeGeneratorTest.java
@@ -0,0 +1,42 @@
+package edu.umb.cs680.hw02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.Test;
+
+class PrimeGeneratorTest {
+
+	@Test
+	public void oneToTen() {
+		PrimeGenerator gen = new PrimeGenerator(1, 10);
+		gen.generatePrimes();
+		Long[] expectedPrimes = {2L, 3L, 5L, 7L};
+		assertArrayEquals( expectedPrimes,gen.getPrimes().toArray());
+				
+	}
+	
+	@Test
+	public void tenTo20() {
+		PrimeGenerator gen = new PrimeGenerator(10,20);
+		gen.generatePrimes();
+		Long[] expectedPrimes = {11L, 13L, 17L, 19L};
+		assertArrayEquals( expectedPrimes,gen.getPrimes().toArray());
+		
+		
+	}
+
+
+	
+	@Test
+	public void negoneTo10() {
+		PrimeGenerator gen = new PrimeGenerator(-5,-1);
+		gen.generatePrimes();
+		Long[] expectedPrimes = {};
+		assertArrayEquals( expectedPrimes,gen.getPrimes().toArray());	
+	
+	}
+	
+	
+}
