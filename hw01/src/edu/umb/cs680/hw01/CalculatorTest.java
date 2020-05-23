bf43b428e2487d494a583209779d6ded82ab355c
Apoorva0516 <38570180+Apoorva0516@users.noreply.github.com>
2020-05-23 15:57:00 -0400

    Add files via upload

---
+++ hw01/src/edu/umb/cs680/hw01/CalculatorTest.java
@@ -0,0 +1,31 @@
+package edu.umb.cs680.hw01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+
+
+class CalculatorTest {
+
+	@Test
+	public void multiply7and8() {
+		Calculator cal = new Calculator();
+		float actual = cal.multiply(7,8);
+		float expected = 56;
+		assertEquals(actual,expected);
+		
+	}
+	
+	
+	@Test
+	public void divide72by9() {
+		Calculator cal= new Calculator();
+		float actual = cal.divide(72, 9);
+		float expected = 8;
+		assertEquals(actual,expected);
+	}
+	
+
+}
